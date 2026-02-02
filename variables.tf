a472102e588820c26a46e32521586b7d6d0a0b15
Vamsi Krishna <vamsi.krishna@tensure.io>
2026-02-02 19:50:36 +0530

    added starter null resource

---
+++ variables.tf
@@ -0,0 +1,33 @@
+
+
+variable "project_id" {
+    description = "The GCP Project ID"
+    type        = string
+    default = "vamsi-krishna-sandbox"
+}
+
+variable "region" {
+  description = "GCP region to deploy resources to."
+  default     = "us-central1"
+}
+
+variable "github_app_id" {
+  description = "github app id"
+  
+}
+
+variable "github_app_client_id" {
+  description = "github app client id"
+}
+
+variable "github_app_client_secret" {
+  description = "github app client secret"
+}
+variable "github_app_private_key" {
+  description = "github app private key"
+}
+variable "github_app_webhook_secret" {
+  description = "github app webhook secret"
+}
+
+
---
+++ versions.tf
@@ -0,0 +1,27 @@
+terraform {
+  backend "gcs" {
+    bucket  = " atlantis-vamsi-state-bucket"
+  }
+  required_providers {
+    google = {
+      source  = "hashicorp/google"
+      version = "5.16.0"
+    }
+    kubernetes = {
+      source  = "hashicorp/kubernetes"
+      version = ">= 2.29.0, < 3.0.0"
+    }
+    helm = {
+      source  = "hashicorp/helm"
+      version = "2.15.0"
+    }
+  }
+}
+
+provider "google" {
+  project = var.project_id
+  region = var.region
+}
+
+
+
