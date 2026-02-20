0d7f0fe91af37796471c0a52aa12dc22338dba65
Vamsi Krishna <vamsi.krishna@tensure.io>
2026-02-20 14:38:24 +0530

    aDDED gcs resource

--- main.tf
+++ main.tf
@@ -14,3 +14,17 @@ resource "google_storage_bucket" "pivora-gcs" {
     env = "dev"
   }
 }
+
+resource "google_storage_bucket" "pivora-gcs-2" {
+  name          = "atlantis-test-resource-vamc-2"
+  project       = var.project_id
+  location      = "US"
+  force_destroy = true
+
+  uniform_bucket_level_access = true
+
+  labels = {
+    app = "atlantis"
+    env = "dev"
+  }
+}
