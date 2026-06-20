3e9248778423d0e75d03493fa1236074a933df3f
Vamsi Krishna <vamsi.krishna@tensure.io>
2026-06-20 15:32:56 +0530

    modified

--- main.tf
+++ main.tf
@@ -1,2 +1,16 @@
 
 resource "null_resource" "example" {}
+
+resource "google_storage_bucket" "pivora-gcs-3" {
+  name          = "atlantis-test-resource-vamc-3"
+  project       = var.project_id
+  location      = "US"
+  force_destroy = true
+
+  uniform_bucket_level_access = true
+
+  labels = {
+    app = "atlantis"
+    env = "dev"
+  }
+}
